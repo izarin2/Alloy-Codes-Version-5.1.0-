c6e316b06df84f6b6fde56a54f56f3cd10890ae3
izarin2 <izarin2@huskers.unl.edu>
2024-03-31 14:34:56 -0500

    upload

--- MergeCSV.sh
+++ MergeCSV.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-csv_file1="/home/isratzarin/Documents/test2.csv"
-csv_file2="/home/isratzarin/Documents/test2.csv"
-output_file="/home/isratzarin/Documents/test3.csv"
-
-# Join the files based on the first column (assuming it's a unique identifier)
-join -t ',' -j 1 "$csv_file1" "$csv_file2" > "$output_file"
-
-# Rename the output columns for clarity (assuming 2nd and 3rd columns are to be kept from each file)
-sed -i '1s/[^,]*/&_file1/' "$output_file"  # Rename 2nd and 3rd column from first file
-sed -i '1s/[^,]*/&_file2/' "$output_file"  # Rename 2nd and 3rd column from second file
-
-
-#!/bin/bash
-
 var1="/home/isratzarin/Desktop/Test/examples/algorithms/dijkstra.als"
 # var2="Minisat"
 solvers=("Minisat" "Sat4j" "MiniSatProver" "CryptoMiniSatJNI" "Glucose" "Glucose41JNI" "KK" "Lingeling")
